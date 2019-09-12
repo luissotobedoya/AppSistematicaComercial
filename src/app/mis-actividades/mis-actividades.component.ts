34797731707a315aaf621fe82e344d2b496ff14e
Esteban Botero <estebanbotero1036@gmail.com>
2019-09-12 09:50:55 -0500

    modificacion obtenerActividades

--- src/app/mis-actividades/mis-actividades.component.ts
+++ src/app/mis-actividades/mis-actividades.component.ts
@@ -89,9 +89,9 @@ export class MisActividadesComponent implements OnInit {
   }
 
   ObtenerActividadesUsuarioActual() {
-    // let fechaActual = this.ObtenerFormatoFecha(new Date());
-    let fechaActual = "2019-09-12";
-    this.listaRespuestas = "RespuestasActividades201909"
+    let fechaActual = this.ObtenerFormatoFecha(new Date());
+    // let fechaActual = "2019-09-12";
+    // this.listaRespuestas = "RespuestasActividades201909"
     this.servicio.obtenerActividadesDelDia(this.listaRespuestas, this.usuarioActual.id, this.usuarioActual.rol, fechaActual).subscribe(
       (Response) => {
         this.coleccionRespuestasActividadesUsuario = Respuesta.fromJsonList(Response);
